a2e89b2a56fd0f4baec236e7b7a280edcfeb083a
Subhangi-2216 <lamichhanesubhangi67@gmail.com>
2024-05-01 15:10:09 +0545

    completed bottomnavbar(user side) - first commit

---
+++ lib/views/buyers/main_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        // type: BottomNavigationBarType.fixed,
+        currentIndex: _pageIndex,
+        onTap: (value){
+          setState(() {
+            _pageIndex = value;
+          });
+        },
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.yellow.shade900,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/explore.svg',
+              width: 20,
+            ),
+            label: 'CATEGORIES',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/shop.svg',
+              width: 20,
+            ),
+            label: 'STORE',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/cart.svg',
+              width: 20,
+            ),
+            label: 'CART',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/search.svg',
+              width: 20,
+            ),
+            label: 'SEARCH',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/account.svg',
+              width: 20,
+            ),
+            label: 'ACCOUNT',
+          ),
+        ],
+      ),
+    );
+  }
+}
